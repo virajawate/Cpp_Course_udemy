c95714a5a4a25ac9a04f9929339e6a73b6b5f980
Viraj Awate <viraj.awate@gmail.com>
2023-07-02 23:48:11 +0530

    exception_handling

---
+++ 06 File Handling/exception_handling.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, m, q;
+    cout<<"Enter the numbers : ";
+    cin>>n>>m;
+    try{
+        if(n>0 && m>0)
+        {
+            q = n / m;
+            cout<<q<<endl;
+        }
+        else{
+            throw m;
+        }
+    }
+    catch(int x)
+    {
+        cout<<"Exception : m is "<<m<<endl;
+    }
+
+
+    return 0;
+}
