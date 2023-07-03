e70c9e38d7d17c1dc3736e635d1ec75b69149fa4
Viraj Awate <viraj.awate@gmail.com>
2023-07-03 16:18:39 +0530

    Standard_Temp_Library

---
+++ 07 Standard Tempelate Libraries/Array.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+    array<int, 6> arr;
+    array<int, 6> A1 = {10, 30, 10, 24 };
+
+    cout << "Second element in A1 :" << A1.at(2) << endl;
+    cout << "Third element in A1 :" << A1[2] << endl;
+    cout << "Enter the elements in the array :" << endl;
+
+    for (int i = 0; i < 6; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Second element in arr :" << arr.at(2) << endl;
+    cout << "Third element in arr :" << arr[2] << endl;
+    cout << "Size of the array :" << arr.size() << endl;
+    cout << "Capacity of the array :" << arr.max_size() << endl;
+
+    arr.swap(A1); // Size of both arrays should be same
+    arr.empty();
+    cout << "=:arr is emptyed:=" << endl;
+    cout << "Size of the array :" << arr.size() << endl;
+    cout << "Capacity of the array :" << arr.max_size() << endl;
+
+    cout << "Second element in A1 :" << A1.at(2) << endl;
+    cout << "Third element in A1 :" << A1[2] << endl;
+    cout << "Size of the A1 :" << A1.size() << endl;
+    cout << "Capacity of the A1 :" << A1.max_size() << endl;
+
+    return 0;
+}
---
+++ 07 Standard Tempelate Libraries/Vector.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> Vec1;
+    vector<int> Vec2 = {10, 32, 53};
+
+    Vec1.push_back(23);
+    Vec1.push_back(405);
+    Vec1.push_back(42);
+    Vec1.push_back(99);
+
+    cout << "Size of Vector1 :" << Vec1.size() << endl;
+
+    cout << "At 3 : " << Vec1.at(3) << endl;
+    cout << "[] : " << Vec1[3] << endl;
+
+    Vec2.push_back(34);
+    Vec2.push_back(459);
+    cout << "First Vector : " << endl;
+    for (vector<int>::iterator i = Vec1.begin(); i != Vec1.end(); i++)
+    {
+        cout << *i << "  ";
+    }
+
+    cout << endl;
+
+    vector<int>::iterator j = Vec1.begin();
+    Vec1.insert(j, 11);
+    cout << "First Vector : " << endl;
+    for (vector<int>::iterator i = Vec1.begin(); i != Vec1.end(); i++)
+    {
+        cout << *i << "  ";
+    }
+    cout << endl;
+    cout << "Second Vector : " << endl;
+    for (vector<int>::iterator i = Vec2.begin(); i != Vec2.end(); i++)
+    {
+        cout << *i << "  ";
+    }
+    cout << endl;
+    Vec2.pop_back();
+    cout << "After pop back " << endl;
+    cout << "Second Vector : " << endl;
+    for (vector<int>::iterator i = Vec2.begin(); i != Vec2.end(); i++)
+    {
+        cout << *i << "  ";
+    }
+    cout << endl;
+    cout<<"Size of Vec1 : "<<Vec1.size()<<" Size of Vec2 : "<<Vec2.size()<<endl;
+    cout << "Capacity of Vec1 : " << Vec1.capacity() << " Capacity of Vec2 :" << Vec2.capacity() << endl;
+    return 0;
+}
